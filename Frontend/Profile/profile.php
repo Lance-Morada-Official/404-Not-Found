0baee373de70eb79be24631b72d9414e4b2c75bf
LanceMorada <lanc3r.m0rad@gmail.com>
2024-10-04 17:51:57 +0800

    Profile Again OPPA

--- Frontend/Profile/profile.php
+++ Frontend/Profile/profile.php
@@ -8,46 +8,31 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="profile.css"/>
     <link rel="stylesheet" href="../Navigation/navigation_bar.css">
-    <title>Navigation Bar Test</title>
-</head>
-<body class="vh-100 overflow-hidden">
-    <?php include '../Navigation/navigation_bar.php'; ?>
-    <?php
-// Assume connection to the database is already established
-session_start();
-
-// Get user information from the database
-$user_id = $_SESSION['user_id']; // Assuming user is logged in and their ID is stored in session
-
-$query = "SELECT username, firstname, lastname, email, phone_num FROM _user WHERE user_id = ?";
-$stmt = $conn->prepare($query);
-$stmt->bind_param('i', $user_id);
-$stmt->execute();
-$stmt->bind_result($username, $firstname, $lastname, $email, $phone_num);
-$stmt->fetch();
-$stmt->close();
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="profile.css"/>
-    <link rel="stylesheet" href="../Navigation/navigation_bar.css">
     <title>User Profile</title>
 </head>
 <body class="vh-100 overflow-hidden">
     <?php include '../Navigation/navigation_bar.php'; ?>
+    <?php
+    // Assume connection to the database is already established
+    session_start();
+
+    // Get user information from the database
+    $user_id = $_SESSION['user_id']; 
+
+    $query = "SELECT username, firstname, lastname, email, phone_num FROM _user WHERE user_id = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param('i', $user_id);
+    $stmt->execute();
+    $stmt->bind_result($username, $firstname, $lastname, $email, $phone_num);
+    $stmt->fetch();
+    $stmt->close();
+    ?>
 
     <div class="container mt-5">
         <div class="row">
             <!-- Left Side: User Information -->
             <div class="col-md-4">
-                <div class="card shadow-sm p-3 mb-5 bg-white rounded">
+                <div class="card shadow-sm p-3 mb-5 profile-card">
                     <!-- Profile Picture -->
                     <form action="upload_profile_pic.php" method="POST" enctype="multipart/form-data">
                         <div class="text-center mb-3">
@@ -72,7 +57,5 @@ $stmt->close();
             </div>
         </div>
     </div>
-</body>
-</html>
 </body>
 </html>
