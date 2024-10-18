d3924ff87abcbdba81ee19a627d218408e3cdf76
PMaraan <maraanpatrick@gmail.com>
2024-10-18 22:33:25 +0800

    Updated friends.php
    Updated the friends  system. The system should now show a dynamic list of friends.

--- Frontend/Friends/friends.php
+++ Frontend/Friends/friends.php
@@ -1,3 +1,7 @@
+<?php
+    require_once('..\..\Backend\include\session-include.php');
+    require_once('..\..\Backend\include\dbconnect-include.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,18 +12,30 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="friends.css"/>
     <link rel="stylesheet" href="..\Navigation\navigation_bar.css">
-    <title>Navigation Bar Test</title>
+    <title>Friends</title>
 </head>
 <body class="vh-100 overflow-hidden">
     <?php 
-	include '..\..\Frontend\Navigation\navigation_bar.php'; 
-	/*session_start();
-	if (!isset($_SESSION['username'])) {  // Check if the user is logged in by checking the session variable
-		die("You must be logged in to view your details.");
-	}
-	require('..\..\Backend\include\dbconnect-include.php');
-		
-	$connect->close();*/
+	include '..\..\Frontend\Navigation\navigation_bar.php';
+    //get a list of the user's friends
+    $sql = "Select Fuser_id from _friendsystem where BINARY Muser_id = '$_SESSION['userid']'";
+	$result = $connect->query($sql);
+    if ($result->num_rows > 0) {
+        ?>
+        <h1>Friends List</h1>
+        <?php
+        while($row = mysqli_fetch_assoc($result)){
+            ?>
+            <div>
+                <h3><?php $row['Fuser_id'] ?>;</h3>
+            </div>
+            <?php
+        }
+    }else {
+        echo "0 results";
+    }
+	//*		
+	$connect->close();//*/
 	?>   
 </body>
 </html>
