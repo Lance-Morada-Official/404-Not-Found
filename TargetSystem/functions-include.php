727829daa0c8f6d56a5b583d520678e560dba322
PMaraan <maraanpatrick@gmail.com>
2024-11-01 22:42:19 +0800

    Create decrypt function
    Created a decrypt function in functions-include.php for the target system.

--- TargetSystem/functions-include.php
+++ TargetSystem/functions-include.php
@@ -1,16 +1,30 @@
 <?php
 
 
-    function encryptMessage($data, $publicKey){
+    function encryptData($data, $publicKey){
         // Get the public key .pem file
         $publicKey = file_get_contents('resources/securepay_public_key.pem');
 
         // Encrypt the data
-        openssl_public_encrypt($data, $encryptedMessage, $publicKey);
+        openssl_public_encrypt($data, $encryptedData, $publicKey);
 
-        // Save or display the encrypted message
-        $encryptedMessageBase64 = base64_encode($encryptedMessage);
-        echo "Encrypted Message: " . $encryptedMessageBase64 . "\n";
+        // Save the encrypted data to base64 to survive transport layers that are not 8-bit clean
+        $encryptedDataBase64 = base64_encode($encryptedData);
+        echo "Encrypted Data: " . $encryptedDataBase64 . "\n";
+    }
+
+    function decryptData($privateKey, $encryptedDataBase64){
+        // Get the private key .pem file
+        $privateKey = file_get_contents('resources/securepay_private_key.pem');
+
+        // Decode the base64 encrypted data
+        $encryptedData = base64_decode($encryptedDataBase64);
+
+        // Decrypt the data
+        openssl_private_decrypt($encryptedData, $decryptedData, $privateKey);
+
+        // Display the decrypted data
+        echo "Decrypted Data: " . $decryptedData . "\n";
     }
     
 ?>
