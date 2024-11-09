fda5586e4645de7aa91314a34146de8bb703ba57
LanceMorada <lanc3r.m0rad@gmail.com>
2024-11-09 08:29:33 +0800

    INVITE FOLDER INTO TRANSACTIONS

--- Backend/include/dbconnect-include.php
+++ Backend/include/dbconnect-include.php
@@ -3,6 +3,6 @@
 $connect = new mysqli('localhost:3306', 'root', '', 'BCDatabase');
 
 if ($connect->connect_error) {
-  die("Connection failed: " . $connect->connect_error);
+ die("Connection failed: " . $connect->connect_error);
 }
 ?>
