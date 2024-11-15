0718d231f61940f40ec4e6f55cfcc15c861652e3
PMaraan <maraanpatrick@gmail.com>
2024-11-15 23:48:48 +0800

    Added Encrypt-Decrypt Functionality to Client
    Created public and private keys, added encrypt and decrypt functions to ChainGuard.

--- Backend/include/functions-include.php
+++ Backend/include/functions-include.php
@@ -68,4 +68,34 @@ function Inserttouser($connect,$username,$firstname,$lastname, $confirm_password
 	//close statement
 	$stmt->close();
 }
+
+//encrypt
+function encryptData($data, $publicKey){
+	// Get the public key .pem file
+	$publicKey = file_get_contents('resources/chainguard_public_key.pem');
+
+	// Encrypt the data
+	openssl_public_encrypt($data, $encryptedData, $publicKey);
+
+	// Save the encrypted data to base64 to survive transport layers that are not 8-bit clean
+	$encryptedDataBase64 = base64_encode($encryptedData);
+	echo "Encrypted Data: " . $encryptedDataBase64 . "\n";
+}
+
+//decrypt
+function decryptData($encryptedDataBase64, $privateKey){
+	// Get the private key .pem file
+	$privateKey = file_get_contents('resources/chainguard_private_key.pem');
+
+	// Decode the base64 encrypted data
+	$encryptedData = base64_decode($encryptedDataBase64);
+
+	// Decrypt the data
+	openssl_private_decrypt($encryptedData, $decryptedData, $privateKey);
+
+	// Display the decrypted data
+	echo "Decrypted Data: " . $decryptedData . "\n";
+}
+
+//request server public key
 ?>
