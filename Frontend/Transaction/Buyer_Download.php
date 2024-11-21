6519135fcad5accabf5bde888364653377b0f2a7
LanceMorada <lanc3r.m0rad@gmail.com>
2024-11-21 20:55:37 +0800

    TRADE SUCCESS

--- Frontend/Transaction/Buyer_Download.php
+++ Frontend/Transaction/Buyer_Download.php
@@ -5,23 +5,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="Buyer_Preview.css" />
+    
+    <link rel="stylesheet" href="Buyer_Download.css" />
     <link rel="stylesheet" href="Details.css" />
-    <link rel="stylesheet" href="Loading.css" />
     <link rel="stylesheet" href="BoxContainer.css" />
     <link rel="stylesheet" href="../Navigation/navigation_bar.css" />
-    <title>Waiting for Preview</title>
+    
+    <title>Trade Success | Download</title>
 </head>
 
 <body class="vh-100 overflow-hidden" style="font-family: 'Poppins', sans-serif; background-color: #161530;">
+    
     <?php include '../../Frontend/Navigation/navigation_bar.php'; ?>
 
     <div class="container-fluid">
         <div class="row">
             
-            <!-- Left Column for Buyer Details -->
             <div class="col-lg-2 mt-3 mb-3 ms-3 Details">
                 <div class="Details-Header">
                     <h2>Trading With</h2>
@@ -56,28 +59,37 @@
             </div>
 
             <div class="col-lg-9 mt-3 ms-4 BoxContainer">
-                
-            <div class = "Loading-Text mt-2">
-                    <h1>Waiting for Seller to Upload.</h1>
+                <h1>Download</h1>
+                <div class="mx-2 UploadBox">
+                    <h6>File to Download Displays Here.</h6>
+                </div>
+                <div class="mt-2">
+                    <label for="file-title">Title</label>
+                    <input type="text" id="file-title" class="form-control" readonly placeholder="No file selected" />
+                </div>
 
-                 
-                    <div class="spinner-container">
-                     
-                        <div class="spinner-grow text-light" role="status">
-                            <span class="visually-hidden">Loading...</span>
-                        </div>
-                        <div class="spinner-grow text-light" role="status">
-                            <span class="visually-hidden">Loading...</span>
-                        </div>
-                        <div class="spinner-grow text-light" role="status">
-                            <span class="visually-hidden">Loading...</span>
-                        </div>
+                <div class="mt-2">
+                    <label for="file-size">Size</label>
+                    <input type="text" id="file-size" class="form-control" readonly placeholder="No file selected" />
+                </div>
 
-                        </div>
-                        <button type="button" id="btn-home" class="btn btn-secondary">Home</button>
+                <div class="mt-2">
+                    <label for="file-type">Type</label>
+                    <input type="text" id="file-type" class="form-control" readonly placeholder="No file selected" />
+                </div>
+
+                <div class="mb-3">
+                    <label for="user-paragraph" class="form-label">Notes:</label>
+                    <textarea id="user-paragraph" class="form-control" rows="6" placeholder="Type your paragraph(s) here..." readonly></textarea>
+               </div>
+
+                <div class="mb-3">
+                    <button type="button" id="btn-download" class="btn btn-success">Download</button>
+                </div>
             </div>
         </div>
-        </div>
     </div>
+
 </body>
+
 </html>
--- Frontend/Transaction/Seller_Payment_Success.php
+++ Frontend/Transaction/Seller_Payment_Success.php
@@ -5,23 +5,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="Buyer_Preview.css" />
+    
+    <link rel="stylesheet" href="Seller_Payment_Success.css" />
     <link rel="stylesheet" href="Details.css" />
-    <link rel="stylesheet" href="Loading.css" />
     <link rel="stylesheet" href="BoxContainer.css" />
     <link rel="stylesheet" href="../Navigation/navigation_bar.css" />
-    <title>Waiting for Preview</title>
+    
+    <title>Trade Success</title>
 </head>
 
 <body class="vh-100 overflow-hidden" style="font-family: 'Poppins', sans-serif; background-color: #161530;">
+    
     <?php include '../../Frontend/Navigation/navigation_bar.php'; ?>
 
     <div class="container-fluid">
         <div class="row">
             
-            <!-- Left Column for Buyer Details -->
             <div class="col-lg-2 mt-3 mb-3 ms-3 Details">
                 <div class="Details-Header">
                     <h2>Trading With</h2>
@@ -56,28 +59,19 @@
             </div>
 
             <div class="col-lg-9 mt-3 ms-4 BoxContainer">
-                
-            <div class = "Loading-Text mt-2">
-                    <h1>Waiting for Seller to Upload.</h1>
-
-                 
-                    <div class="spinner-container">
-                     
-                        <div class="spinner-grow text-light" role="status">
-                            <span class="visually-hidden">Loading...</span>
-                        </div>
-                        <div class="spinner-grow text-light" role="status">
-                            <span class="visually-hidden">Loading...</span>
-                        </div>
-                        <div class="spinner-grow text-light" role="status">
-                            <span class="visually-hidden">Loading...</span>
-                        </div>
-
-                        </div>
-                        <button type="button" id="btn-home" class="btn btn-secondary">Home</button>
+            <div class="mt-3 mb-3 mx-3 SuccessContainer">
+            <h1 class="Trade-Success-Text">Trade Success!</h1>
+                <div class="mb-3">
+                    <label for="TradePayment">Received:</label>
+                    <input type="number" id="TradePayment" name="TradePayment" placeholder="00.00" readonly>
+                </div>
+                <button type="button" id="btn-home" class="btn btn-success">Home</button>
             </div>
-        </div>
+
+                
         </div>
     </div>
+
 </body>
+
 </html>
